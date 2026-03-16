fa08348cdcb63a68218b804114d0692d294731b9
yeaobo <yeaobo123@163.com>
2026-03-16 09:44:05 +0800

    Update base path for llmdemo

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src')
     }
   },
-  base: process.env.NODE_ENV === 'production' ? '/overseas-llm2/' : './',
+  base: process.env.NODE_ENV === 'production' ? '/llmdemo/' : './',
   server: {
     port: 3000,
     open: true
